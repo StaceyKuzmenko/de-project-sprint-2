c2b6f522b995962c62f27b739b47bb5a00f81d0f
Anastasiya Kuzmenko <113375513+StaceyKuzmenko@users.noreply.github.com>
2023-06-29 19:19:33 +0300

    Update shipping_agreement.sql

--- shipping_agreement.sql
+++ shipping_agreement.sql
@@ -1,16 +1,31 @@
-DROP TABLE IF EXISTS public.shipping_agreement;
-
+DROP 
+  TABLE IF EXISTS public.shipping_agreement;
 CREATE TABLE public.shipping_agreement(
-  agreement_id BIGINT NOT NULL PRIMARY KEY,
-  agreement_number varchar(10) NOT NULL,
-  agreement_rate numeric(14, 2) NOT NULL,
+  agreement_id BIGINT NOT NULL PRIMARY KEY, 
+  agreement_number varchar(10) NOT NULL, 
+  agreement_rate numeric(14, 2) NOT NULL, 
   agreement_commission numeric(14, 2) NOT NULL
-  );
-
-INSERT INTO public.shipping_agreement (agreement_id, agreement_number, agreement_rate, agreement_commission)
-SELECT DISTINCT
-  split_part(vendor_agreement_description, ':', 1)::BIGINT as agreement_id,
-  split_part(vendor_agreement_description, ':', 2)::varchar(10) as agreement_number,
-  split_part(vendor_agreement_description, ':', 3)::numeric(14, 2) as agreement_rate,
-  split_part(vendor_agreement_description, ':', 4)::numeric(14, 2) as agreement_commission 
-FROM public.shipping;
+);
+INSERT INTO public.shipping_agreement (
+  agreement_id, agreement_number, agreement_rate, 
+  agreement_commission
+) 
+SELECT 
+  DISTINCT split_part(
+    vendor_agreement_description, ':', 
+    1
+  ):: BIGINT as agreement_id, 
+  split_part(
+    vendor_agreement_description, ':', 
+    2
+  ):: varchar(10) as agreement_number, 
+  split_part(
+    vendor_agreement_description, ':', 
+    3
+  ):: numeric(14, 2) as agreement_rate, 
+  split_part(
+    vendor_agreement_description, ':', 
+    4
+  ):: numeric(14, 2) as agreement_commission 
+FROM 
+  public.shipping;
