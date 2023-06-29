525b4549a47483e39b8d59f99406de541b24dbc4
Anastasiya Kuzmenko <113375513+StaceyKuzmenko@users.noreply.github.com>
2023-06-29 19:22:27 +0300

    Update shipping_status.sql

--- shipping_status.sql
+++ shipping_status.sql
@@ -1,43 +1,55 @@
-DROP TABLE IF EXISTS public.shipping_status;
-
-CREATE TABLE public.shipping_status(
-  shipping_id int8 NOT NULL PRIMARY KEY,
-  status text NULL,
-  state text NULL,
-  shipping_start_fact_datetime timestamp NULL,
-  shipping_end_fact_datetime timestamp NULL
-  );
-
-INSERT INTO public.shipping_status (shipping_id, status, state, shipping_start_fact_datetime, shipping_end_fact_datetime)
-SELECT DISTINCT
-  s.shippingid::int8 AS shipping_id,
-  st.status::text,
-  st.state::text,
-  sfd.shipping_start_fact_datetime::timestamp,
-  efd.shipping_end_fact_datetime::timestamp
-FROM public.shipping s
-LEFT JOIN (SELECT 	srt.shippingid as shipping_id,
-					srt.status,
-					srt.state
-		FROM (SELECT DISTINCT 	shippingid,
-			 					status,
-								state,
-		 						NTILE(7) OVER (PARTITION BY shippingid ORDER BY state_datetime DESC) AS sort
-  			FROM public.shipping) as srt
-			WHERE sort = 1
-		  ) AS st
-ON s.shippingid=st.shipping_id
-LEFT JOIN (SELECT 	shippingid AS shipping_id,
-		   			state_datetime AS shipping_start_fact_datetime
-					--NTILE(1) OVER (ORDER BY state_datetime ASC) AS shipping_transfer_description
-  	   	   FROM public.shipping
-		   WHERE state = 'booked'
-		  ) AS sfd
-ON s.shippingid=sfd.shipping_id
-LEFT JOIN (SELECT 	shippingid AS shipping_id,
-		   			state_datetime AS shipping_end_fact_datetime
-					--NTILE(1) OVER (ORDER BY state_datetime ASC) AS shipping_transfer_description
-  	   	   FROM public.shipping
-		   WHERE state = 'recieved'
-		  ) AS efd
-ON s.shippingid=efd.shipping_id;
+DROP 
+  TABLE IF EXISTS public.shipping_info;
+CREATE TABLE public.shipping_info(
+  shipping_id int8 NOT NULL PRIMARY KEY, 
+  vendor_id int8 NULL, 
+  payment_amount numeric(14, 2) NULL, 
+  shipping_plan_datetime timestamp NULL, 
+  shipping_transfer_id int8 NOT NULL, 
+  shipping_agreement_id int8 NULL, 
+  shipping_country_rate_id int8 NOT NULL, 
+  FOREIGN KEY(shipping_transfer_id) REFERENCES public.shipping_transfer(id) ON UPDATE cascade, 
+  FOREIGN KEY(shipping_agreement_id) REFERENCES public.shipping_agreement(agreement_id) ON UPDATE cascade, 
+  FOREIGN KEY(shipping_country_rate_id) REFERENCES public.shipping_country_rates(id) ON UPDATE cascade
+);
+INSERT INTO public.shipping_info (
+  shipping_id, vendor_id, payment_amount, 
+  shipping_plan_datetime, shipping_transfer_id, 
+  shipping_agreement_id, shipping_country_rate_id
+) 
+SELECT 
+  DISTINCT s.shippingid :: int8 AS shipping_id, 
+  s.vendorid :: int8 AS vendor_id, 
+  s.payment_amount :: numeric(14, 2) AS payment_amount, 
+  s.shipping_plan_datetime :: timestamp AS shipping_plan_datetime, 
+  st.shipping_transfer_id :: int8, 
+  sa.shipping_agreement_id :: int8, 
+  scr.shipping_country_rate_id :: int8 
+FROM 
+  public.shipping s 
+  LEFT JOIN (
+    SELECT 
+      id AS shipping_transfer_id, 
+      concat(
+        transfer_type, ':', transfer_model
+      ) AS shipping_transfer_description 
+    FROM 
+      public.shipping_transfer
+  ) AS st ON s.shipping_transfer_description = st.shipping_transfer_description 
+  LEFT JOIN (
+    SELECT 
+      agreement_id AS shipping_agreement_id, 
+      concat(
+        agreement_id, ':', agreement_number, 
+        ':', agreement_rate, ':', agreement_commission
+      ) AS vendor_agreement_description 
+    FROM 
+      public.shipping_agreement
+  ) AS sa ON s.vendor_agreement_description = sa.vendor_agreement_description 
+  LEFT JOIN (
+    SELECT 
+      id AS shipping_country_rate_id, 
+      shipping_country 
+    FROM 
+      public.shipping_country_rates
+  ) AS scr ON s.shipping_country = scr.shipping_country;
